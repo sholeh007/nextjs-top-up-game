60784a3371f102209279f82e4074257138926cea
sholeh007 <sholeh09@outlook.com>
2021-11-25 15:54:06 +0900

    convert page checkout html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/homepage.css";
 import "../styles/utilities.css";
 import "../styles/detail.css";
+import "../styles/checkout.css";
 import "aos/dist/aos.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
