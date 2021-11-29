e5cccd00134e3926df2ad8700f597971703cc3c7
sholeh007 <sholeh09@outlook.com>
2021-11-29 09:55:42 +0900

    refactoring feature-game page & fix bug style image thumbnail

---
+++ components/molecules/game-items/index.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface GameProps {
+  id: number;
+  link: string;
+  image: string;
+  icon: string;
+  title: string;
+  category: string;
+}
+
+export default function GameItems({
+  id,
+  link,
+  image,
+  icon,
+  title,
+  category,
+}: GameProps) {
+  return (
+    <div className="featured-game-card position-relative">
+      <Link href={`/${link}/${id}`} passHref>
+        <a>
+          <div className="blur-sharp">
+            <Image
+              src={`/img/${image}.png`}
+              width={205}
+              height={270}
+              alt="thumbnail"
+              className="thumbnail"
+            />
+          </div>
+          <div className="cover position-absolute bottom-0 m-32">
+            <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
+              <div className="game-icon mx-auto">
+                <Image
+                  src={`/icon/${icon}.svg`}
+                  alt="icon"
+                  width={54}
+                  height={36}
+                />
+              </div>
+              <div>
+                <p className="fw-semibold text-white text-xl m-0">{title}</p>
+                <p className="fw-light text-white m-0">{category}</p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </Link>
+    </div>
+  );
+}
---
+++ components/organisms/feature-game/index.tsx
@@ -0,0 +1,34 @@
+import GameItems from "../../molecules/game-items";
+import GameList from "./game-list";
+
+export default function FeatureGames() {
+  const gameList = GameList();
+
+  return (
+    <section className="featured-game pt-50 pb-50">
+      <div className="container-fluid">
+        <h2 className="text-4xl fw-bold color-palette-1 mb-30">
+          Our Featured
+          <br /> Games This Year
+        </h2>
+        <div
+          className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
+          data-aos="fade-up"
+        >
+          {gameList.map((item, i) => (
+            <div key={i}>
+              <GameItems
+                id={item.id}
+                link={item.link}
+                image={item.image}
+                icon={item.icon}
+                title={item.title}
+                category={item.category}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
